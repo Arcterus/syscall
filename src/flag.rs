50a1b6fb66dad09d154d517dd99c3ad6250026e2
Alex Lyon <arcterus@mail.com>
2017-12-16 01:27:51 -0800

    Add F_SETPIPE_READFD for pipe2()

--- src/flag.rs
+++ src/flag.rs
@@ -17,6 +17,7 @@ pub const F_GETFD: usize = 1;
 pub const F_SETFD: usize = 2;
 pub const F_GETFL: usize = 3;
 pub const F_SETFL: usize = 4;
+pub const F_SETPIPE_READFD: usize = 5;
 
 pub const FUTEX_WAIT: usize = 0;
 pub const FUTEX_WAKE: usize = 1;
